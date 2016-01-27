b5c89c4eef5b5c53e897026b4d1a3685a39d9222
SavionLegendZzz <saviondeavault@gmail.com>
2016-01-27 16:39:18 -0500

    Update Main.php

--- src/PS/Main.php
+++ src/PS/Main.php
@@ -12,20 +12,21 @@ class Main extends PluginBase implements Listener {
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("PS §aLoaded");
     }
 
 	public function onLoad(){
-		$this->getLogger()->info("PS §aenabled");
+		$this->getLogger()->info("PS §aLoading....");
 	}
 
     public function onHit(EntityDamageEvent $event){
         $entity = $event->getEntity();
+        if($event instanceof EntityDamageByEntityEvent){
         $fizz = new AnvilBreakSound($entity);
         $entity->getLevel()->addSound($fizz);
      }
-	
+}
     public function onDisable(){
         $this->getLogger()->info("PS §cdisabled.");
-        return true;
 	}
 }
